d973c53a1445a68c39483bd6ccf7a2acb3efac06
Anna Dodson <Anna.Dodson@DVSA.gov.uk>
2021-04-14 13:29:41 +0100

    Refactor try catch and code tidy
    - Address code review comments, tidy up some variables and comments
    that weren't required
    - Consolidate two try catch blocks into one and handle different
    type of errors thrown

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -11,11 +11,9 @@ import { AvailabilityChangeData } from '../types';
 
 export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
   const logger: Logger = createLogger(null, context);
+  const config = getConfig();
   logger.info('Confirmation email lambda triggered.');
 
-  const config = getConfig();
-
-  // Fetch templates from S3
   logger.info('Fetching email templates from S3...');
   const [err, templates] = await handle(getEmailTemplates({
     awsRegion: config.awsRegion,
@@ -24,13 +22,14 @@ export const handler = async (event: SQSEvent, context: Context): Promise<void> 
     fullyBookedTemplate: config.fullyBookedEmailTemplateS3Object,
     endpoint: config.emailTemplateS3Endpoint,
   }));
+
   if (err) {
     logger.error(`An error occurred while fetching the email templates from S3: ${err.message}`);
     throw err;
   }
+
   const { availableTemplate, fullyBookedTemplate } = templates;
 
-  // Loop through each record
   const records = event.Records;
   const emailMessages: EmailMessageRequest[] = [];
   logger.info(`Received ${records.length} records from DynamoDB.`);
@@ -38,24 +37,20 @@ export const handler = async (event: SQSEvent, context: Context): Promise<void> 
     logger.debug(`Processing record: ${JSON.stringify(record)}`);
 
     let message: AvailabilityChangeData;
+    let availabilityData: AvailabilityChangeData;
     try {
       const body: Record<string, string> = <Record<string, string>> JSON.parse(record.body);
       message = JSON.parse(body.Message) as AvailabilityChangeData;
-    } catch (parseError) {
-      logger.error(`Error while parsing message body for messageId: ${record.messageId}`);
-      logger.error(parseError);
+      availabilityData = validateAvailabilityData(message);
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        logger.error(`Error while parsing message body for messageId: ${record.messageId}`);
+        logger.error(error.message);
+        return;
+      }
+      logger.error((error as Error).message);
       return;
     }
-
-    // Extract data from event
-    let availabilityData: AvailabilityChangeData;
-    try {
-      availabilityData = validateAvailabilityData(message);
-    } catch (err2) {
-      logger.error((err2 as Error).message);
-      return; // skip to next record
-    }
-    const { availability } = availabilityData;
 
     const emailMessageRequest = buildSQSMessage({
       queueUrl: config.queueUrl,
@@ -67,22 +62,23 @@ export const handler = async (event: SQSEvent, context: Context): Promise<void> 
         token: availabilityData.token,
         availableTemplate,
         fullyBookedTemplate,
-        availability,
+        availability: availabilityData.availability,
         emailLinkBaseUrl: config.emailLinkBaseUrl,
       },
     });
+
     emailMessages.push(emailMessageRequest);
   });
 
   if (emailMessages.length) {
-    const [err3] = await handle(enqueueEmailMessages({
+    const [emailError] = await handle(enqueueEmailMessages({
       emailMessages,
       awsRegion: config.awsRegion,
       logger,
     }));
-    if (err3) {
-      logger.error(`Error while enqueueing email message(s): ${err3.message}`);
-      throw err3;
+    if (emailError) {
+      logger.error(`Error while enqueueing email message(s): ${emailError.message}`);
+      throw emailError;
     }
   }
 
